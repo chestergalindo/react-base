524a8167a9e992268534f4013d9d7e00b0052600
César Galindo <csgalindos@hotmail.com>
2020-04-26 12:46:39 -0500

    [update] graphic initial

--- src/components/GeneralContainer.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export default function GeneralContainer() {
-  return <div>general</div>;
-}
---
+++ src/components/molecules/GraphicContainer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../styles/components/molecules/graphicContainer.styl';
+
+export default function GraphicContainer() {
+  return (
+    <div className="graphic">
+      <div className="graphicContainer" />
+    </div>
+  );
+}
